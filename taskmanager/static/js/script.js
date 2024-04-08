d64ace6ed997c331b7edd22e7788f0b3e45984bc
rachaelbabister <rachaelbabister@gmail.com>
2024-04-08 21:12:17 +0000

    Create add_task functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,16 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidebar initialisation
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener('DOMContentLoaded', function () {
+  // sidebar initialisation
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
 
-    // datepicker initialisation
-    let datepicker = document.querySelectorAll('.datepicker');
-    M.Datepicker.init(datepicker, {
-      format: "dd mmm, yyyy",
-      i18n: {done: "Select"}
-    });
-
-    // select initialisation
-    let selects = document.querySelectorAll('select');
-    M.FormSelect.init(selects);
+  // datepicker initialisation
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmm, yyyy",
+    i18n: { done: "Select" }
   });
+
+  // select initialisation
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+
+  // collapsible initilisation
+  let collapsibles = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsibles);
+
+});
