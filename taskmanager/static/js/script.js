7a8fc6fd2642a4b28e8580bc98fa1f910cb3a71f
rachaelbabister <rachaelbabister@gmail.com>
2024-04-08 20:42:50 +0000

    Add new tasks function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidebar initialisation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialisation
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // select initialisation
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
